d0c7cb3ac6ea5f5e1b595050330f3f900870d7a6
Vidish D <contentvidish@gmail.com>
2026-01-29 23:56:20 +0530

    feat: 3D parallax archive card with aqulade branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,74 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
+
+export default function Home(){
+  const x = useMotionValue(0)
+  const y = useMotionValue(0)
+  const mouseX = useSpring(x, {stiffness: 150, damping: 20})
+  const mouseY = useSpring(y, {stiffness: 150, damping: 20})
+  const rotateX = useTransform(mouseY, [-0.5, 0.5], [10, -10])
+  const rotateY = useTransform(mouseX, [-0.5, 0.5], [-10, 10])
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    const rect = e.currentTarget.getBoundingClientRect()
+
+    const width = rect.width
+    const height = rect.height
+
+    const mouseXPos = e.clientX - rect.left;
+    const mouseYPos = e.clientY - rect.top;
+
+    x.set(mouseXPos / width - 0.5)
+    y.set(mouseYPos / height - 0.5)
+  }
+  
+  return(
+    <main className="min-h-screen bg-linear-to-r from-[#000000] to-[#050505] flex items-center justify-center p-4 perspective-[1000px]">
+
+      {/* The Card Container */}
+      <motion.div
+      
+      onMouseMove={handleMouseMove}
+
+      onMouseLeave={() => { x.set(0); y.set(0);}}
+      
+      style={{rotateX, rotateY, transformStyle: "preserve-3d"}}
+      className="w-full max-w-2xl border border-zinc-400 bg-linear-to-r from-[#000000] to-[#303030] rounded-2xl p-12 flex flex-col items-center text-center space-y-8 will-change-transform">
+
+        {/* The Logo */}
+        <div style={{transform: "translateZ(50px)", transformStyle: "preserve-3d"}}>
+        <h2 className="text-white drop-shadow-[0_0_10px_rgba(255,255,255,0.8)] text-5xl font-ahsing tracking-tight lowercase">
+          aqulade
+        </h2>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        {/* The Main Text */}
+        <h1 className="text-white drop-shadow-[0_0_20px_rgba(255,255,255,0.8)] text-7xl font-horizon leading-none">
+            Coming <br />Soon
+        </h1>
+        
+
+        {/* The Tag Line */}
+        <p className="text-lg text-zinc-400 font-light max-w-xs">
+          Because everyone deserves to wear thier feelings.
+        </p>
+
+        {/* The Button */}
+        <div className="space-y-2" style={{transform: "translateZ(50px)", transformStyle: "preserve-3d"}}> 
+        <button className="bg-white text-black px-10 py-3 font-medium text-sm rounded-full hover:bg-zinc-200 transition-colors uppercase tracking-widest">
+          Notify Me
+        </button>
         </div>
-      </main>
-    </div>
-  );
+
+      </motion.div>
+
+      {/* Social Icon */}
+      <div className="absolute bottom-10">
+        <div className="w-8 h-8 border border-white rounded-md flex items-center justify-center">
+          <span className="text-white text-sm">IG</span>
+        </div>
+      </div>
+    </main>
+  )
 }
